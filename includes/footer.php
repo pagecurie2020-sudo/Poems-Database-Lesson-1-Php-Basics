91065ed393a99d78c14774954b4ab5c8e46bb0e0
hammock1 <farasylvain1977@hotmail.com>
2025-08-27 07:23:30 -0400

    My 1st commit /Chapt 1

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer>
+  <span>Copyright &copy; 2024 The Poet Tree Club.</span>
+  <nav>
+    <a href="logout.php">Log out</a>
+    <a href="admin/index.php">Admin</a>
+    <a href="about-us.php">About us</a>
+  </nav>
+</footer>
